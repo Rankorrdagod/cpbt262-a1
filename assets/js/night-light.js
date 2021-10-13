c7083a1d4bf1243d3467a3a729c44843a35a64af
Evan Burton <butonevan1@gmaail.com>
2021-10-13 08:44:38 -0600

    added some js

---
+++ assets/js/night-light.js
@@ -0,0 +1,20 @@
+ // Wait for document to load
+ document.addEventListener("DOMContentLoaded", function(event) {
+    document.documentElement.setAttribute("data-theme", "light");
+
+    // Get our button switcher
+    const themeSwitcher = document.getElementById("theme-switcher");
+
+    // When our button gets clicked
+    themeSwitcher.onclick = function() {
+      // Get the current selected theme, on the first run
+      // it should be `light`
+      const currentTheme = document.documentElement.getAttribute("data-theme");
+
+      // Switch between `dark` and `light`
+      const switchToTheme = currentTheme === "dark" ? "light" : "dark"
+
+      // Set our currenet theme to the new one
+      document.documentElement.setAttribute("data-theme", switchToTheme);
+    }
+  });
