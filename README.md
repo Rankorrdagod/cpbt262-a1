d6acecff7f759b9bcbbd94227c33e3d4af0a7ec5
Evan Burton <butonevan1@gmaail.com>
2021-10-13 09:10:42 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# cpbt262-a1
+# cpnt262-a1
+## Dark/Light Mode
+ Create a clickable button using js to switch a webpage between dark and light modes.
+ - Set up file structure, index.html, assets/js, assets/css.
+ - Thinking of trying to make the old gallery page the index for this assignment.
+ - Apparently css wont accept let and const as keywords? Need to use var?
+ - Have used const in the JS portion and var in css and things seem to be working okay. Ask Tony about this.
+ - Switch is working so far on a basic page with nothing on it. Want to change the button to something nicer looking.
+ - Wondering if theme change will swap colors on SVG elements as well... Will add logo to see.
--- assets/js/night-light.js
+++ assets/js/night-light.js
@@ -1,17 +1,18 @@
- // Wait for document to load
+
+ //Doc Load
  document.addEventListener("DOMContentLoaded", function(event) {
     document.documentElement.setAttribute("data-theme", "light");
 
-    // Get our button switcher
+    // button switcher
+
     const themeSwitcher = document.getElementById("theme-switcher");
 
     // When our button gets clicked
     themeSwitcher.onclick = function() {
-      // Get the current selected theme, on the first run
-      // it should be `light`
+     // default light theme first
       const currentTheme = document.documentElement.getAttribute("data-theme");
 
-      // Switch between `dark` and `light`
+      // Switch between dark/light
       const switchToTheme = currentTheme === "dark" ? "light" : "dark"
 
       // Set our currenet theme to the new one
